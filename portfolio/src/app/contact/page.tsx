879c6968a842b9bbf126fbe0bb78cab7fcfbc33f
Eero Tarri <eero.tarri@icloud.com>
2024-02-27 17:45:29 +0200

    Cleanup

--- portfolio/src/app/contact/page.tsx
+++ portfolio/src/app/contact/page.tsx
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="text-6xl font-bold">Hello, World!</h1>
-        <p className="text-2xl font-light">
-          Welcome to my portfolio. I'm a web developer.
-        </p>
+        <h1 className="text-6xl font-bold">Contact Page</h1>
       </div>
     </main>
   );
